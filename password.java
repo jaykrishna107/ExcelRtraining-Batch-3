25a9514f30e1683ec1405667b5a933e9d2d0a00c
jaykrishna107 <168705314+jaykrishna107@users.noreply.github.com>
2025-01-02 15:02:35 +0530

    verifying password with scanner class

---
+++ password.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class password {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your password: ");
+        String password = sc.nextLine(); 
+
+        if (password.equals("user_001")) { 
+            System.out.println("Hello User, Happy New Year!");
+        } else {
+            System.out.println("Incorrect Password");
+        }
+
+        sc.close();
+    }
+}
