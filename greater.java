bdf420313d1bdf536fa434f9709dd38342856a32
jaykrishna107 <168705314+jaykrishna107@users.noreply.github.com>
2025-01-02 14:20:43 +0530

    if else - greater of two numbers

---
+++ greater.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class greater{
+    public static void main(String []args){
+        int a =59;
+        int b =20;
+        if (a > b){
+            System.out.println(a+" is greater than "+b);
+        if(b > a){
+            System.out.println(b+" is greater than "+a);
+        }
+        }
+    }
+}
