04b1533d8e692ce686fbe035e01f4da47713ff91
jaykrishna107 <168705314+jaykrishna107@users.noreply.github.com>
2025-01-24 12:49:38 +0530

    user input

---
+++ Demo7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Demo7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your Age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter your Contact: ");
+        String contact = sc.nextLine();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Contact: " + contact);
+
+        sc.close();
+    }
+}
