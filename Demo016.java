389c8f5ccce01604eb274a19e08b9185f3f54554
jaykrishna107 <168705314+jaykrishna107@users.noreply.github.com>
2025-01-24 12:54:53 +0530

    Palindrome of 3

---
+++ Demo016.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class PalindromeOf3 {
+    boolean isPalindromeOf3Digits(int n){
+        return (n / 100) == (n % 10);
+    }
+}
+
+public class Demo016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PalindromeOf3 obj = new PalindromeOf3();
+        System.out.print("Enter a 3-digit number: ");
+        int n = sc.nextInt();
+        System.out.println(obj.isPalindromeOf3Digits(n));
+    }
+}
