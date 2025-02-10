062cebdfc508ee07a61ea798be62066506c8325f
jaykrishna107 <168705314+jaykrishna107@users.noreply.github.com>
2025-02-10 12:40:25 +0530

    sum of 2 elements in an array

---
+++ Demo063.java
@@ -0,0 +1,9 @@
+public class Demo063 {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 20, 30, 40, 50 };
+        int add = arr[0]+arr[arr.length-1];
+                System.out.println("Sum is: "+add);
+        }
+}  
