f3b85bcf95f8c7998c21e5dcddea38de548b306f
jaykrishna107 <168705314+jaykrishna107@users.noreply.github.com>
2025-02-10 12:42:29 +0530

    printing even numbers in an array

---
+++ Demo067.java
@@ -0,0 +1,12 @@
+public class Demo067 {
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 20, 30, 40, 50 };
+        for(int i = 0; i<arr.length; i++){
+                if(arr[i] %2 == 0){
+                        System.out.println("The even elements in an array is"+arr[i]);
+                }
+        }
+     }
+}
+ 
