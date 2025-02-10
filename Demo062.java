091bd9aaad3e71139e7d72ec810f97b1d1575397
jaykrishna107 <168705314+jaykrishna107@users.noreply.github.com>
2025-02-10 12:39:39 +0530

    First num and last num of an array

---
+++ Demo062.java
@@ -0,0 +1,10 @@
+public class Demo062 {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 20, 30, 40, 50 };
+        System.out.println("First element is :" + arr[0] + " second element is: "
+                                + arr[1] + " last element is: " + arr[arr.length - 1]);
+
+            }
+        }
