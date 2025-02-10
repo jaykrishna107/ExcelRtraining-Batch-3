ba7086d82ec32b8527f22d41c1d8db26c7d3a980
jaykrishna107 <168705314+jaykrishna107@users.noreply.github.com>
2025-02-10 12:38:20 +0530

    counting prime number in array

---
+++ Demo060.java
@@ -0,0 +1,16 @@
+public class Demo060 {
+    public static void main(String[] args) {
+        System.out.println("Enter a Number to find fibonacci series: ");
+        int n = 20;
+        System.out.println("Fibonacci series up to " + n + " terms:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
